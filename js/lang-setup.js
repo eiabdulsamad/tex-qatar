729571d839a6287dfc9f258a816d6f9b1eb0324c
Abdul Samad <abdul.samad@mdrift.in>
2016-05-01 11:25:22 +0530

    updated

---
+++ js/lang-setup.js
@@ -0,0 +1,53 @@
+
+$(function() {
+
+//language setup
+
+function placeholderLanguage() {
+  var placeholders = document.querySelectorAll('input[placeholder], textarea[placeholder]');
+  if (placeholders.length) {
+    placeholders = Array.prototype.slice.call(placeholders);
+    var div = $('<div id="placeholders" style="display:none;"></div>');
+    placeholders.forEach(function(input){
+      var text = input.placeholder;
+      div.append('<div>' + text + ' </div>');    
+    });
+    $('body').append(div);
+    var originalPH = placeholders[0].placeholder;
+    setInterval(function(){
+      if (isTranslated()) {
+        updatePlaceholders();
+        originalPH = placeholders[0].placeholder;
+      }
+    }, 0);
+    function isTranslated() {
+      var currentPH = $($('#placeholders > div')[0]).text();
+      return !(originalPH == currentPH);
+    }
+    function updatePlaceholders() {
+      $('#placeholders > div').each(function(i, div){
+        placeholders[i].placeholder = $(div).text();
+      });
+    }
+  }
+}
+
+$(window).load(function() {
+  if($('body').hasClass('pace-done')) {
+    setTimeout(function() {
+    languageSelector("Arabic");
+     },100)
+}
+});
+
+
+function languageSelector(lang) {
+  var $frame = $('.goog-te-menu-frame:first');
+  $frame.contents().find('.goog-te-menu2-item span.text:contains('+lang+')').get(0).click();
+}
+
+Pace.on('hide', function(){
+  placeholderLanguage();
+});
+
+});
