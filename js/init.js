2b162217c8c3f232f0c51a98aff9a8c18a3720f5
Abdul Samad <abdul.samad@mdrift.in>
2016-05-01 08:50:46 +0530

    up

--- js/init.js
+++ js/init.js
@@ -34,13 +34,10 @@ getCoverValue();
 // ---------start menu---------
 
 
-var myVar = setInterval(function(){
-      if($('body').hasClass('pace-done')) {
-        generateMenu();
-        menuSmart();
-        clearInterval(myVar);
-      }
-    }, 100);
+Pace.on('hide', function(){
+  generateMenu();
+  menuSmart();
+});
 
 
 /*Pace.on('hide', function(){
@@ -174,34 +171,6 @@ function getUrlVars() {
 
 //Language setup
 
-function placeholderLanguage() {
-  var placeholders = document.querySelectorAll('input[placeholder], textarea[placeholder]');
-  if (placeholders.length) {
-    placeholders = Array.prototype.slice.call(placeholders);
-    var div = $('<div id="placeholders" style="display:none;"></div>');
-    placeholders.forEach(function(input){
-      var text = input.placeholder;
-      div.append('<div>' + text + '</div>');    
-    });
-    $('body').append(div);
-    var originalPH = placeholders[0].placeholder;
-    setInterval(function(){
-      if (isTranslated()) {
-        updatePlaceholders();
-        originalPH = placeholders[0].placeholder;
-      }
-    }, 0);
-    function isTranslated() {
-      var currentPH = $($('#placeholders > div')[0]).text();
-      return !(originalPH == currentPH);
-    }
-    function updatePlaceholders() {
-      $('#placeholders > div').each(function(i, div){
-        placeholders[i].placeholder = $(div).text();
-      });
-    }
-  }
-}
 
 $(window).load(function() {
   var footerScroll = new Swiper('.footer-scroll', {
@@ -215,19 +184,7 @@ $(window).load(function() {
 
 
 
-function languageSelector(lang) {
-    var $frame = $('.goog-te-menu-frame:first');
-    $frame.contents().find('.goog-te-menu2-item span.text:contains('+lang+')').get(0).click();
-  }
 
-
-var myVar = setInterval(function(){
-      if($('body').hasClass('pace-done')) {
-        languageSelector("Arabic");
-        placeholderLanguage();
-        clearInterval(myVar);
-      }
-    }, 100);
 
   //var getLang = getUrlVars()["lang"];
   //alert(lang);
--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(function() {
   rating('.rating-box');
 
 
+
+
 $(document).on('click', '.open-product', function(){
   var dataID = $(this).attr('data-id');
   
@@ -80,10 +82,10 @@ if((window.location.href.indexOf('?')+1) && (! $('body').hasClass('home'))) {
   if($('body').hasClass('products')) {
     var dataID = getUrlVars()["i"];
 
-    var myVar = setInterval(function(){
+    var myVar3 = setInterval(function(){
       if($('body').hasClass('pace-done')) {
         listView(dataID);
-        clearInterval(myVar);
+        clearInterval(myVar3);
       }
     }, 100);
       
@@ -501,7 +503,55 @@ function closePopUp() {
 
 }
 
-});
 
- 
+
+
+
+
+function placeholderLanguage() {
+  var placeholders = document.querySelectorAll('input[placeholder], textarea[placeholder]');
+  if (placeholders.length) {
+    placeholders = Array.prototype.slice.call(placeholders);
+    var div = $('<div id="placeholders" style="display:none;"></div>');
+    placeholders.forEach(function(input){
+      var text = input.placeholder;
+      div.append('<div>' + text + '</div>');    
+    });
+    $('body').append(div);
+    var originalPH = placeholders[0].placeholder;
+    setInterval(function(){
+      if (isTranslated()) {
+        updatePlaceholders();
+        originalPH = placeholders[0].placeholder;
+      }
+    }, 0);
+    function isTranslated() {
+      var currentPH = $($('#placeholders > div')[0]).text();
+      return !(originalPH == currentPH);
+    }
+    function updatePlaceholders() {
+      $('#placeholders > div').each(function(i, div){
+        placeholders[i].placeholder = $(div).text();
+      });
+    }
+  }
+}
+
+
+
+      if($('body').hasClass('pace-done')) {
+        languageSelector("Arabic");
+      }
+
+      function languageSelector(lang) {
+    var $frame = $('.goog-te-menu-frame:first');
+    $frame.contents().find('.goog-te-menu2-item span.text:contains('+lang+')').get(0).click();
+  }
+  
+Pace.on('hide', function(){
+  placeholderLanguage();
+});
+  
+
+});
  
